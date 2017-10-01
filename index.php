31c23059ccd331d451981aa88c1ba26276aca4a5
Skynance <nan141979@hotmail.com>
2017-09-30 21:47:54 -0300

    adicionei template admin e configurei

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
+
 $app = new Slim();
 
 $app->config('debug', true);
@@ -13,6 +15,11 @@ $app->get('/', function() {
 
 });
 
+$app->get('/admin', function() {
+    $page = new PageAdmin();
+    $page-> setTpl("index");
+
+});
  $app->run();
 
  ?>
